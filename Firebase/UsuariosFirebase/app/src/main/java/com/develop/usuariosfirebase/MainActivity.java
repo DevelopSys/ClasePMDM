cd580cbe33ed4ed4460acac8beb5c9d8cc518d76
develop <developandsys@gmail.com>
2020-03-29 15:38:49 +0200

    update

---
+++ Firebase/UsuariosFirebase/app/src/main/java/com/develop/usuariosfirebase/MainActivity.java
@@ -0,0 +1,86 @@
+package com.develop.usuariosfirebase;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText nombreLog, passLog, nombreSign, passSign;
+    private Button btnLog, btnSign;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        nombreLog = findViewById(R.id.edit_usuario_log);
+        nombreSign = findViewById(R.id.edit_usuario_sig);
+        passLog = findViewById(R.id.edit_pass_log);
+        passSign = findViewById(R.id.edit_pass_sig);
+        btnLog = findViewById(R.id.button_log);
+        btnSign = findViewById(R.id.button_sign);
+        btnLog.setOnClickListener(this);
+        btnSign.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.button_log:
+                mAuth.signInWithEmailAndPassword(nombreLog.getText().toString(), passLog.getText().toString())
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d("login", "signInWithEmail:success");
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w("login", "signInWithEmail:failure", task.getException());
+                                    Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                break;
+            case R.id.button_sign:
+
+                mAuth.createUserWithEmailAndPassword(nombreSign.getText().toString(), passSign.getText().toString())
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d("login", "createUserWithEmail:success");
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w("login", "createUserWithEmail:failure", task.getException());
+                                    Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                break;
+        }
+    }
+}
