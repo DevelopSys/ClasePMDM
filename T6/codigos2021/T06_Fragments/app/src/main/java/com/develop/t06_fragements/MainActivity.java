42730fa7a968890f245b0e21bc6a6f2a7c20b548
develop <developandsys@gmail.com>
2021-01-14 10:07:21 +0100

    update

--- T6/codigos2021/T06_Fragments/app/src/main/java/com/develop/t06_fragements/MainActivity.java
+++ T6/codigos2021/T06_Fragments/app/src/main/java/com/develop/t06_fragements/MainActivity.java
@@ -1,6 +1,7 @@
 package com.develop.t06_fragements;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -15,11 +16,14 @@ import android.widget.Toast;
 import com.develop.t06_fragements.fragments.FragmentDos;
 import com.develop.t06_fragements.fragments.FragmentUno;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button boton, bF1, bF2, bF3;
     FragmentManager fragmentManager;
     FragmentTransaction fragmentTransaction;
+    int cuentaFg = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,25 +63,57 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         fragmentManager = getSupportFragmentManager();
         fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out);
+        FragmentManager.BackStackEntry backStackEntry = null;
+        if (cuentaFg != 0) {
+            backStackEntry =
+                    fragmentManager.getBackStackEntryAt(fragmentManager.getBackStackEntryCount() - 1);
+        }
 
 
         switch (v.getId()) {
             // en cada case se realiza la acción
             case R.id.button_ir_f1:
-                fragmentTransaction.replace(R.id.sitio_fragments, new FragmentUno());
-                fragmentTransaction.addToBackStack("f1");
+                fragmentTransaction.add(R.id.sitio_fragments, new FragmentUno(), "f1");
+                if (cuentaFg > 0) {
+                    Log.v("prueba_fg", String.valueOf(backStackEntry.getName()));
+                    if (!backStackEntry.getName().equals("f1")) {
+                        fragmentTransaction.addToBackStack("f1");
+                    }
+                } else {
+                    fragmentTransaction.addToBackStack("f1");
+                }
+                cuentaFg++;
 
                 break;
             case R.id.button_ir_f2:
-                fragmentTransaction.replace(R.id.sitio_fragments, new FragmentDos());
-                fragmentTransaction.addToBackStack("f2");
+                fragmentTransaction.add(R.id.sitio_fragments, new FragmentDos(), "f2");
+                if (cuentaFg > 0) {
+                    Log.v("prueba_fg", String.valueOf(backStackEntry.getName()));
+                    if (!backStackEntry.getName().equals("f2")) {
+                        fragmentTransaction.addToBackStack("f2");
+                    }
+                } else {
+                    fragmentTransaction.addToBackStack("f2");
+                }
+                cuentaFg++;
                 break;
             case R.id.button_ir_f3:
                 int cuentaPila = fragmentManager.getBackStackEntryCount();
-                FragmentManager.BackStackEntry backStackEntry =
-                        fragmentManager.getBackStackEntryAt(fragmentManager.getBackStackEntryCount()-1);
 
-                Log.v("prueba_fg",String.valueOf(backStackEntry.getName()));
+                FragmentManager.BackStackEntry backStackEntry2 =
+                        fragmentManager.getBackStackEntryAt(fragmentManager.getBackStackEntryCount() - 1);
+
+                Log.v("prueba_fg", "Etiqueta estado " + String.valueOf(backStackEntry2.getName()));
+                Log.v("prueba_fg", "Numero estados " + String.valueOf(cuentaPila));
+                Log.v("prueba_fg", "Numero fragments " + String.valueOf(fragmentManager.getFragments().size()));
+                ArrayList<Fragment> listaFragments = (ArrayList<Fragment>) fragmentManager.getFragments();
+                for (Fragment fragment:listaFragments) {
+                    Log.v("prueba_fg", "Fragment Apilado " + String.valueOf(fragment));
+                }
+                Fragment fragmentEncontrado = getSupportFragmentManager().findFragmentByTag("f2");
+                Log.v("prueba_fg", "FragmentEncontrado " + String.valueOf(fragmentEncontrado));
+
+
                 break;
         }
 
@@ -97,3 +133,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }*/
     }
 }
+
+/*
+* PILA --> BackStack --> Estados {Como está la pila_: qu eelementos hay en la pila}
+* inicio pila: nada 0 estados
+* replace bf1: (solo con el replace, el addToBackStack "f1" {f1} y el commit) 1 estados --> fragment que se ve: f1
+* replace bf2: (solo con el replace, el addToBackStack "f2" {f2} y el commit) 2 estados --> fragment que se ve: f2
+* si el estado anterior es f2, no quiero hacer el addToBackStack
+* replace bf2: (solo con el replace, y el commit) 2 estados --> fragment que se ve: f2
+* si el estado anterior es f1, no quiero hacer el addToBackStack
+* * replace bf1: (solo con el replace, el addToBackStack "f1" {f1} y el commit) 3 estados --> fragment que se ve: f1
+* * * replace bf1: (solo con el replace, y el commit) 3 estados --> fragment que se ve: f1
+ * */
