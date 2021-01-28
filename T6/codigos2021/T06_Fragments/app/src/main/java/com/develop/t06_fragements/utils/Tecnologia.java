e609584b296520495bc52dae2d7614ad06e59809
develop <developandsys@gmail.com>
2021-01-28 14:31:40 +0100

    update

---
+++ T6/codigos2021/T06_Fragments/app/src/main/java/com/develop/t06_fragements/utils/Tecnologia.java
@@ -0,0 +1,37 @@
+package com.develop.t06_fragements.utils;
+
+public class Tecnologia {
+
+    String nombre, descripcion;
+    int imagen;
+
+    public Tecnologia(String nombre, String descripcion, int imagen) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.imagen = imagen;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(int imagen) {
+        this.imagen = imagen;
+    }
+}
