0e7134db5b8f8e3f3be6cbe619fa3f400972a3c3
develop <developandsys@gmail.com>
2021-02-04 10:06:02 +0100

    update

--- T7/codigos2021/T07_NavigationView/app/src/main/java/com/borja/t07_navigationview/MainActivity.java
+++ T7/codigos2021/T07_NavigationView/app/src/main/java/com/borja/t07_navigationview/MainActivity.java
@@ -1,14 +1,85 @@
 package com.borja.t07_navigationview;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private SearchView barraBusqueda;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        instancias();
+        configurarBarra();
+        acciones();
+
+
+    }
+
+    private void acciones() {
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()) {
+                    case R.id.menu_opcion_uno:
+                        Toast.makeText(getApplicationContext(), "Pulsado 1", Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawers();
+                        break;
+                    case R.id.menu_opcion_dos:
+                        Toast.makeText(getApplicationContext(), "Pulsado 2", Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawers();
+                        break;
+                    case R.id.menu_opcion_tres:
+                        Toast.makeText(getApplicationContext(), "Pulsado 3", Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawers();
+                        break;
+
+                }
+
+                return true;
+            }
+        });
+    }
+
+
+    private void configurarBarra() {
+        this.setSupportActionBar(toolbar);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(MainActivity.this, drawerLayout, toolbar
+                , R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+    }
+
+    private void instancias() {
+        toolbar = findViewById(R.id.toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.menu_navigation);
+        barraBusqueda = (SearchView) navigationView.getMenu().getItem(R.id.menu_opcion_busqueda).getActionView();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //getMenuInflater().inflate(R.menu.menu_lateral,menu);
+        return true;
     }
 }
