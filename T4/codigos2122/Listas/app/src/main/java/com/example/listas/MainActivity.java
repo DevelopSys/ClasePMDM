22835c826776c4140fafaec5a80362f954cb658c
Develop and System <developandsystem@gmail.com>
2021-11-17 10:48:52 +0100

    update

--- T4/codigos2122/Listas/app/src/main/java/com/example/listas/MainActivity.java
+++ T4/codigos2122/Listas/app/src/main/java/com/example/listas/MainActivity.java
@@ -6,8 +6,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import com.example.listas.utils.Marca;
 
 import java.util.ArrayList;
 
@@ -15,9 +18,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private Spinner spinnerCoches;
     private TextView textoCoches;
-    private Button botonComprobar;
+    private ImageView imagenCoche;
+    private Button botonComprobar, botonAnadir;
     private ArrayAdapter adaptadorSpinner;
-    private ArrayList<String> listaMarcas;
+    private ArrayList<Marca> listaMarcas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +35,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void acciones() {
         botonComprobar.setOnClickListener(this);
+        botonAnadir.setOnClickListener(this);
     }
 
     private void iniciarElementos() {
@@ -38,17 +43,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void iniciarListas() {
-        listaMarcas.add("Mercedes");
-        listaMarcas.add("Audi");
-        listaMarcas.add("BMW");
-        listaMarcas.add("VW");
-        listaMarcas.add("Ford");
+        listaMarcas.add(new Marca("Mercedes",R.drawable.mercedes));
+        listaMarcas.add(new Marca("Audi",R.drawable.audi));
+        listaMarcas.add(new Marca("BMW",R.drawable.bmw));
+        listaMarcas.add(new Marca("VW",R.drawable.vw));
+
     }
 
     private void instancias() {
         spinnerCoches = findViewById(R.id.spinner_coches);
-        textoCoches = findViewById(R.id.texto_coche);
+        //textoCoches = findViewById(R.id.texto_coche);
+        imagenCoche = findViewById(R.id.imagen_coche);
         botonComprobar = findViewById(R.id.boton_coches);
+        botonAnadir = findViewById(R.id.boton_anadir);
         listaMarcas = new ArrayList();
         adaptadorSpinner = new ArrayAdapter(getApplicationContext(),
                 android.R.layout.simple_spinner_item, listaMarcas);
@@ -63,9 +70,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 //textoCoches.setText(String.valueOf(spinnerCoches.getSelectedItem()));
                 // adaptadorSpinner
                 //spinnerCoches.getAdapter()
-                textoCoches.setText(
+                /*textoCoches.setText(
                         adaptadorSpinner.
-                                getItem(spinnerCoches.getSelectedItemPosition()).toString());
+                                getItem(spinnerCoches.getSelectedItemPosition()).toString());*/
+                Marca marcaSeleccionada =
+                        (Marca) adaptadorSpinner.getItem(spinnerCoches.getSelectedItemPosition());
+
+                imagenCoche.setImageResource(marcaSeleccionada.getImagen());
+
+                break;
+
+            case R.id.boton_anadir:
+
+                listaMarcas.add(new Marca("MarcaNueva",R.drawable.def));
+                adaptadorSpinner.notifyDataSetChanged();
+
 
                 break;
         }
