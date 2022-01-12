199e626cc4dd8671e196c76c870241749143af59
Develop and System <developandsystem@gmail.com>
2022-01-12 18:33:44 +0100

    update

--- T4/codigos2122/T4_Recycler/app/src/main/java/com/example/t4_recycler/MainActivity.java
+++ T4/codigos2122/T4_Recycler/app/src/main/java/com/example/t4_recycler/MainActivity.java
@@ -1,22 +1,65 @@
 package com.example.t4_recycler;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+
+import com.example.t4_recycler.adaptadores.AdaptadorRecycler;
+import com.example.t4_recycler.utils.Contacto;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
+    private AdaptadorRecycler adaptadorRecycler;
+    private ArrayList<Contacto> listaContactos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         instancias();
+        rellenarLista();
+        asociarListas();
+    }
+
+    private void asociarListas() {
+        recyclerView.setAdapter(adaptadorRecycler);
+        /*recyclerView.setLayoutManager(
+                new LinearLayoutManager(getApplicationContext()
+                        ,LinearLayoutManager.VERTICAL,false));*/
+        //recyclerView.setLayoutManager(new GridLayoutManager(getApplicationContext(),3));
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+            //recyclerView.setLayoutManager(new GridLayoutManager(getApplicationContext(),3));
+            recyclerView.setLayoutManager(new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL));
+
+        } else if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){
+            recyclerView.setLayoutManager(
+                new LinearLayoutManager(getApplicationContext()
+                        ,LinearLayoutManager.VERTICAL,false));
+        }
+    }
+
+    private void rellenarLista() {
+        listaContactos.add(new Contacto("Borja","Martin","Pozuelo",1234));
+        listaContactos.add(new Contacto("Pedro","Martin","Getafe",23453));
+        listaContactos.add(new Contacto("Celia","Martin","Alcorcon",5432));
+        listaContactos.add(new Contacto("Marta","Martin","Majadahonda",56456));
+        listaContactos.add(new Contacto("Anaasdasdasdasdasdasdasdasd","Martin","Fuenlabrada",7566));
+        listaContactos.add(new Contacto("Jesus","Martin","Aluche",325643));
+        adaptadorRecycler.notifyDataSetChanged();
     }
 
     private void instancias() {
         recyclerView = findViewById(R.id.recycler_view);
+        listaContactos = new ArrayList<>();
+        adaptadorRecycler = new AdaptadorRecycler(listaContactos,MainActivity.this);
+
     }
 }
--- T4/codigos2122/T4_Recycler/app/src/main/java/com/example/t4_recycler/adaptadores/AdaptadorRecycler.java
+++ T4/codigos2122/T4_Recycler/app/src/main/java/com/example/t4_recycler/adaptadores/AdaptadorRecycler.java
@@ -1,5 +1,7 @@
 package com.example.t4_recycler.adaptadores;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -9,29 +11,43 @@ import androidx.recyclerview.widget.RecyclerView;
 
 
 import com.example.t4_recycler.R;
+import com.example.t4_recycler.utils.Contacto;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+
 public class AdaptadorRecycler extends RecyclerView.Adapter<AdaptadorRecycler.MyHolder> {
 
-    
+    private ArrayList<Contacto> listaContactos;
+    private Context context;
+
+    public AdaptadorRecycler(ArrayList<Contacto> listaContactos, Context context) {
+        this.listaContactos = listaContactos;
+        this.context = context;
+    }
 
     @NonNull
     @Override
     public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-
-        return null;
+        //
+        View view = LayoutInflater.from(context).inflate(R.layout.item_lista,parent,false);
+        return new MyHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyHolder holder, int position) {
+        // se pinta cada una de las filas
+        Contacto contacto = listaContactos.get(position);
+        holder.textoNombre.setText(contacto.getNombre());
+        holder.textoTelefono.setText(String.valueOf(contacto.getTelefono()));
 
     }
 
     @Override
     public int getItemCount() {
 
-        return 0;
+        return listaContactos.size();
     }
 
     class MyHolder extends RecyclerView.ViewHolder {
