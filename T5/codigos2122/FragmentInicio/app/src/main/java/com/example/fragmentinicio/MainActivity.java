5adc07ef002f76682d8c0d288f5ab6b827443c74
Develop and System <developandsystem@gmail.com>
2022-02-17 09:41:48 +0100

    update

--- T5/codigos2122/FragmentInicio/app/src/main/java/com/example/fragmentinicio/MainActivity.java
+++ T5/codigos2122/FragmentInicio/app/src/main/java/com/example/fragmentinicio/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.fragmentinicio;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.fragmentinicio.fragments.FragmentDinamicoDos;
+import com.example.fragmentinicio.fragments.FragmentDinamicoUno;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button bF1, bF2;
+    private FragmentManager fm;
+    private FragmentTransaction ft;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bF1 = findViewById(R.id.boton_f1);
+        bF2 = findViewById(R.id.boton_f2);
+        bF2.setOnClickListener(this);
+        bF1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        fm = getSupportFragmentManager();
+        ft = fm.beginTransaction();
+        ft.setCustomAnimations(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+
+        switch (view.getId()){
+            case R.id.boton_f1:
+                ft.replace(R.id.sitio_fragments,new FragmentDinamicoUno(),"uno");
+                ft.addToBackStack("uno");
+                break;
+            case R.id.boton_f2:
+                ft.replace(R.id.sitio_fragments,new FragmentDinamicoDos(),"dos");
+                ft.addToBackStack("dos");
+                break;
+        }
+
+        ft.commit();
     }
 }
---
+++ T5/codigos2122/FragmentInicio/app/src/main/java/com/example/fragmentinicio/fragments/FragmentDinamicoDos.java
@@ -0,0 +1,24 @@
+package com.example.fragmentinicio.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.fragmentinicio.R;
+
+public class FragmentDinamicoDos extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_dinamico_dos_layout,
+                container,false);
+        return view;
+    }
+}
