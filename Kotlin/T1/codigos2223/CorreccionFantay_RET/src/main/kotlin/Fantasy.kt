3f3dbbf374ed27853cc98336d432b5d958c592a9
Develop and System <developandsystem@gmail.com>
2022-10-11 15:58:40 +0200

    update

--- Kotlin/T1/codigos2223/CorreccionFantay_RET/src/main/kotlin/Fantasy.kt
+++ Kotlin/T1/codigos2223/CorreccionFantay_RET/src/main/kotlin/Fantasy.kt
@@ -2,6 +2,7 @@ class Fantasy {
 
     lateinit var listaParticipante: ArrayList<Participante>;
     lateinit var listaFutbolistas: ArrayList<Jugador>;
+    lateinit var listaPuntuaciones: ArrayList<Puntuacion>;
 
     init {
         listaFutbolistas = ArrayList();
@@ -57,7 +58,81 @@ class Fantasy {
         listaParticipante.forEach ({it.mostrarDatos()})
     }
 
+    fun agregarPunctuaciones(){
+// listaPuntuaciones primera jornada
+        listaPuntuaciones = ArrayList();
+        listaPuntuaciones.add(Puntuacion(1,"Marc-André ter Stegen", 10))
+        listaPuntuaciones.add(Puntuacion(2,"Ronald Araújo",0))
+        listaPuntuaciones.add(Puntuacion(3,"Eric García",3))
+        listaPuntuaciones.add(Puntuacion(4,"Pedri",23))
+        listaPuntuaciones.add(Puntuacion(5,"Robert Lewandowski",15))
+        listaPuntuaciones.add(Puntuacion(6,"Courtois",1))
+        listaPuntuaciones.add(Puntuacion(7,"David Alaba",5))
+        listaPuntuaciones.add(Puntuacion(8,"Jesús Vallejo",10))
+        listaPuntuaciones.add(Puntuacion(9,"Luka Modric",5))
+        listaPuntuaciones.add(Puntuacion(10,"Karim Benzema",10))
+        listaPuntuaciones.add(Puntuacion(11,"Ledesma",6))
+        listaPuntuaciones.add(Puntuacion(12,"Juan Cala",3))
+        listaPuntuaciones.add(Puntuacion(13,"Zaldua",6))
+        listaPuntuaciones.add(Puntuacion(14,"Alez Fernández",9))
+        listaPuntuaciones.add(Puntuacion(15, "Choco Lozano", 4))
+        listaPuntuaciones.add(Puntuacion(16,"Rajković",3))
+        listaPuntuaciones.add(Puntuacion(17,"Raíllo",6))
+        listaPuntuaciones.add(Puntuacion(18,"Maffeo",0))
+        listaPuntuaciones.add(Puntuacion(19,"Ruiz de Galarreta",7))
+        listaPuntuaciones.add(Puntuacion(20,"Ángel", 4))
+        listaPuntuaciones.add(Puntuacion(21,"Remiro",3))
+        listaPuntuaciones.add(Puntuacion(22,"Elustondo",5))
+        listaPuntuaciones.add(Puntuacion(23,"Zubeldia",6))
+        listaPuntuaciones.add(Puntuacion(24,"Zubimendi",7))
+        listaPuntuaciones.add(Puntuacion(25,"Take Kubo", 4))
+// listaPuntuaciones segunda jornada
+        listaPuntuaciones.add(Puntuacion(1,"Marc-André ter Stegen", 5))
+        listaPuntuaciones.add(Puntuacion(2,"Ronald Araújo",5))
+        listaPuntuaciones.add(Puntuacion(3,"Eric García",4))
+        listaPuntuaciones.add(Puntuacion(4,"Pedri",10))
+        listaPuntuaciones.add(Puntuacion(5,"Robert Lewandowski",10))
+        listaPuntuaciones.add(Puntuacion(6,"Courtois",1))
+        listaPuntuaciones.add(Puntuacion(7,"David Alaba",7))
+        listaPuntuaciones.add(Puntuacion(8,"Jesús Vallejo",5))
+        listaPuntuaciones.add(Puntuacion(9,"Luka Modric",15))
+        listaPuntuaciones.add(Puntuacion(10,"Karim Benzema",0))
+        listaPuntuaciones.add(Puntuacion(11,"Ledesma",9))
+        listaPuntuaciones.add(Puntuacion(12,"Juan Cala",3))
+        listaPuntuaciones.add(Puntuacion(13,"Zaldua",3))
+        listaPuntuaciones.add(Puntuacion(14,"Alez Fernández",7))
+        listaPuntuaciones.add(Puntuacion(15, "Choco Lozano", 2))
+        listaPuntuaciones.add(Puntuacion(16,"Rajković",8))
+        listaPuntuaciones.add(Puntuacion(17,"Raíllo",9))
+        listaPuntuaciones.add(Puntuacion(18,"Maffeo",2))
+        listaPuntuaciones.add(Puntuacion(19,"Ruiz de Galarreta",10))
+        listaPuntuaciones.add(Puntuacion(20,"Ángel", 4))
+        listaPuntuaciones.add(Puntuacion(21,"Remiro",5))
+        listaPuntuaciones.add(Puntuacion(22,"Elustondo",8))
+        listaPuntuaciones.add(Puntuacion(23,"Zubeldia",9))
+        listaPuntuaciones.add(Puntuacion(24,"Zubimendi",11))
+        listaPuntuaciones.add(Puntuacion(25,"Take Kubo", 10))
+
+
+
+
+    }
+
     fun iniciarJuego(){
-        listaParticipante.filter ({ it.presupuesto>=0 && it.plantilla.size == 6})
+        agregarPunctuaciones()
+        var participantesValidos = listaParticipante.filter ({ it.presupuesto>=0 && it.plantilla.size == 6})
+        // recorro los participantes
+
+        participantesValidos.forEach({participante: Participante ->
+            // recorro la plantilla del jugador
+            participante.plantilla.forEach({jugador: Jugador->
+                listaPuntuaciones.forEach({puntuacion: Puntuacion ->
+                    if (jugador.id == puntuacion.id){
+                        participante.puntos += puntuacion.puntos;
+                    }
+                })
+            })
+        })
+
     }
 }
