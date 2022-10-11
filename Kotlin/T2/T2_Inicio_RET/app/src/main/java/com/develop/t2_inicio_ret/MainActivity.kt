677a8d21c905f5c560a2a983c69a82bbfa8af9e4
Develop and System <developandsystem@gmail.com>
2022-10-11 17:33:23 +0200

    update

--- Kotlin/T2/T2_Inicio_RET/app/src/main/java/com/develop/t2_inicio_ret/MainActivity.kt
+++ Kotlin/T2/T2_Inicio_RET/app/src/main/java/com/develop/t2_inicio_ret/MainActivity.kt
@@ -2,10 +2,31 @@ package com.develop.t2_inicio_ret
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var botonInicial: Button;
+    lateinit var etiquetaSaludo: TextView;
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+        // asocia la parte lógica y la gráfica
         this.setContentView(R.layout.activity_main)
+        instancias();
+        acciones();
+    }
+
+    private fun acciones() {
+        botonInicial.setOnClickListener({
+            etiquetaSaludo.setText("Hola, primera app completa")
+        })
+    }
+
+    private fun instancias() {
+        this.botonInicial = findViewById(R.id.boton_inicial)
+        this.etiquetaSaludo = findViewById(R.id.texto_saludo);
     }
 }
