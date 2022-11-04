d6cf335d163f2eac8a621365b89745085c90a739
Develop and System <developandsystem@gmail.com>
2022-11-04 10:51:42 +0100

    update

--- Kotlin/T3/codigos2223/calculadora/app/src/main/java/com/develop/calculadora/MainActivity.kt
+++ Kotlin/T3/codigos2223/calculadora/app/src/main/java/com/develop/calculadora/MainActivity.kt
@@ -6,60 +6,66 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import com.develop.calculadora.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-    private lateinit var editNumeros: EditText;
-    private lateinit var botonUno: Button;
-    private lateinit var botonDos: Button;
-    private  var botonTres: Button? = null;
-    private  var botonCuatro: Button? = null;
+    private lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        instancias();
+        //requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view: View = binding.root;
+        setContentView(view)
+        //instancias();
         acciones();
-        if (resources.configuration.orientation == ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE){
+        /*if (resources.configuration.orientation == ActivityInfo.SCREEN_ORIENTATION_PORTRAIT){
             //accionesLand();
-        }
-
+            setContentView(R.layout.activity_main)
+            instancias();
+            acciones();
+        } else if (resources.configuration.orientation == ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
+        {
+            setContentView(R.layout.activity_warning)
+        }*/
     }
 
-    private fun accionesLand() {
+    /*private fun accionesLand() {
         botonTres!!.setOnClickListener(this)
         botonCuatro!!.setOnClickListener(this)
-    }
+    }*/
 
     private fun acciones() {
-        botonUno.setOnClickListener(this)
-        botonDos.setOnClickListener(this)
-        botonTres?.setOnClickListener(this)
-        botonCuatro?.setOnClickListener(this)
+        binding.botonUno .setOnClickListener(this)
+        binding.botonDos.setOnClickListener(this)
+        binding.botonTres?.setOnClickListener(this)
+        binding.botonCuatro?.setOnClickListener(this)
 
     }
 
-    private fun instancias() {
+    /*private fun instancias() {
         botonUno = findViewById(R.id.boton_uno);
         botonDos = findViewById(R.id.boton_dos);
         botonTres = findViewById(R.id.boton_tres);
         botonCuatro = findViewById(R.id.boton_cuatro);
         editNumeros = findViewById(R.id.edit_numeros)
-    }
+    }*/
 
     override fun onClick(p0: View?) {
-        when(p0!!.id){
+        when (p0!!.id) {
             R.id.boton_uno -> {
-                editNumeros.append((p0 as Button).text)
+                binding.editNumeros.append((p0 as Button).text)
             }
             R.id.boton_dos -> {
-                editNumeros.append((p0 as Button).text)
+                binding.editNumeros.append((p0 as Button).text)
             }
             R.id.boton_tres -> {
-                editNumeros.append((p0 as Button).text)
+                binding.editNumeros.append((p0 as Button).text)
             }
             R.id.boton_cuatro -> {
-                editNumeros.append((p0 as Button).text)
+                binding.editNumeros.append((p0 as Button).text)
             }
         }
     }
