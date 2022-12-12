fada2c245f15392a9c6dd4efe68f565d97110070
Develop and System <developandsystem@gmail.com>
2022-12-12 13:56:46 +0100

    Update AdaptadorUsuarios.kt

--- Kotlin/T4/codigos2223/T4_ListasRecycler/app/src/main/java/com/develop/t4_listasrecycler/adapters/AdaptadorUsuarios.kt
+++ Kotlin/T4/codigos2223/T4_ListasRecycler/app/src/main/java/com/develop/t4_listasrecycler/adapters/AdaptadorUsuarios.kt
@@ -1,23 +1,30 @@
 package com.develop.t4_listasrecycler.adapters
 
+import android.content.Context
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.develop.t4_listasrecycler.model.Usuario
 
-class AdaptadorUsuarios: RecyclerView.Adapter<ViewHolder>() {
+class AdaptadorUsuarios(var lista: ArrayList<Usuario>, var contexto: Context)
+    : RecyclerView.Adapter<AdaptadorUsuarios.MyHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        // crea la estructura "hierros" de la fila --> algo (ViewHolder) con un XML
-        TODO("Not yet implemented")
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyHolder {
+        var vista: View = ; // xml aspecto de cada fila
+        return MyHolder(vista);
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        // linkar la fila con el elemento holder concreto
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: MyHolder, position: Int) {
     }
 
     override fun getItemCount(): Int {
-        // cuantas filas tendras que pintar --> size de una lista
-        TODO("Not yet implemented")
+        // tamaño de la lista
+        return lista.size
+    }
+
+    inner class MyHolder(itemView: View) : ViewHolder(itemView) {
+
     }
 }
