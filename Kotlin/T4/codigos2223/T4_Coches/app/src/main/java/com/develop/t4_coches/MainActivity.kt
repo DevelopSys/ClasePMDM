e5c7d5459d72a51099ddeb1a1d237b07d02504df
Develop and System <developandsystem@gmail.com>
2022-12-14 10:03:52 +0100

    update

---
+++ Kotlin/T4/codigos2223/T4_Coches/app/src/main/java/com/develop/t4_coches/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.develop.t4_coches
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.develop.t4_coches.adaptapters.AdapterCoche
+import com.develop.t4_coches.databinding.ActivityMainBinding
+import com.develop.t4_coches.model.Coche
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var coches: ArrayList<Coche>
+    private lateinit var adapterCoche: AdapterCoche
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        coches = ArrayList();
+
+        coches.add(Coche("Mercedes","AMG GT",500,200000,"Deportivo", R.drawable.amggt))
+        coches.add(Coche("Bentley","Continental",400,300000,"Berlina deportivo", R.drawable.continental))
+        coches.add(Coche("Jaguar","FType",300,150000,"Deportivo", R.drawable.ftype))
+        coches.add(Coche("Ford","GT40",500,300000,"Deportivo clasico", R.drawable.gt40))
+        coches.add(Coche("Nissan","GTR",300,200000,"Deportivo", R.drawable.gtr))
+        coches.add(Coche("Porche","Huayara",600,400000,"Deportivo", R.drawable.huayra))
+        coches.add(Coche("Lexus","LC",200,100000,"Deportivo", R.drawable.lc))
+        coches.add(Coche("Ferrari","Le ferrari",600,500000,"Deportivo", R.drawable.leferrari))
+        coches.add(Coche("McLaren","MC600",500,450000,"Deportivo", R.drawable.mc600))
+        coches.add(Coche("Toyota","Supra",300,150000,"Deportivo", R.drawable.supra))
+        coches.add(Coche("Porche","Taycan",350,250000,"Deportivo", R.drawable.taycan))
+
+        adapterCoche = AdapterCoche(this, coches)
+        binding.recycler.adapter = adapterCoche;
+        binding.recycler.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false)
+
+
+    }
+}
---
+++ Kotlin/T4/codigos2223/T4_Coches/app/src/main/java/com/develop/t4_coches/model/Coche.kt
@@ -0,0 +1,3 @@
+package com.develop.t4_coches.model
+
+data class Coche (var marca: String, var modelo: String, var cv: Int, var precio: Int, var tipo: String, var imagen: Int,)
