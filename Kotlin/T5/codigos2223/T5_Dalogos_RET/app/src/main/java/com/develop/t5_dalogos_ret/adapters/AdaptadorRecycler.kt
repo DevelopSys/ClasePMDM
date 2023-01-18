2e7de54e8eb528f01971145873ecc79f2db89ffd
Develop and System <developandsystem@gmail.com>
2023-01-18 17:18:27 +0100

    update

--- Kotlin/T5/codigos2223/T5_Dalogos_RET/app/src/main/java/com/develop/t5_dalogos_ret/adapters/AdaptadorRecycler.kt
+++ Kotlin/T5/codigos2223/T5_Dalogos_RET/app/src/main/java/com/develop/t5_dalogos_ret/adapters/AdaptadorRecycler.kt
@@ -1,4 +1,43 @@
 package com.develop.t5_dalogos_ret.adapters
 
-class AdaptadorRecycler {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.develop.t5_dalogos_ret.R
+import com.develop.t5_dalogos_ret.model.Usuario
+
+class AdaptadorRecycler(var contexto: Context, var lista: ArrayList<Usuario>) :
+    RecyclerView.Adapter<AdaptadorRecycler.MyHolder>() {
+
+    inner class MyHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        var textoNombre: TextView
+        var textoDept: TextView
+
+
+        init {
+            textoNombre = itemView.findViewById(R.id.item_nombre)
+            textoDept = itemView.findViewById(R.id.item_departamento)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyHolder {
+        val vista: View = LayoutInflater.from(contexto)
+            .inflate(R.layout.item_recycler,parent,false)
+        return  MyHolder()
+    }
+
+    override fun onBindViewHolder(holder: MyHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        return lista.size
+    }
+
 }
---
+++ Kotlin/T5/codigos2223/T5_Dalogos_RET/app/src/main/java/com/develop/t5_dalogos_ret/model/Usuario.kt
@@ -0,0 +1,3 @@
+package com.develop.t5_dalogos_ret.model
+
+data class Usuario (var nombre: String, var pass: String, var departamento: String, var recordad: Boolean)
