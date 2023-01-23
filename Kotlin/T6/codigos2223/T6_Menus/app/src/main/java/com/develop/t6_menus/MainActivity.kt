c17a094e015c9e9f2b359b406a1d2a69d48a7f27
Develop and System <developandsystem@gmail.com>
2023-01-23 13:46:55 +0100

    update

--- Kotlin/T6/codigos2223/T6_Menus/app/src/main/java/com/develop/t6_menus/MainActivity.kt
+++ Kotlin/T6/codigos2223/T6_Menus/app/src/main/java/com/develop/t6_menus/MainActivity.kt
@@ -4,28 +4,47 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import com.develop.t6_menus.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.main_menu,menu)
+        menuInflater.inflate(R.menu.main_menu, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
-        when(item.itemId){
-            // sacar un Snackbar en cada uno de los item pulsado
-            // pulsado item1
-            // pulsado item2
-            // pulsado item1.1
-            // pulsado item1.2
-            // pulsado elemento sin recoconcer
+        var texto: String = ""
+
+        when (item.itemId) {
+            R.id.menu_op1 -> {
+                texto = "Elemento 1"
+            }
+            R.id.menu_op2 -> {
+                texto = "Elemento 2"
+            }
+            R.id.menu_op1_1 -> {
+                texto = "Elemento 1_1"
+            }
+            R.id.menu_op1_2 -> {
+                texto = "Elemento 1_2"
+            }
+            else ->{
+                texto = "Elemento no identificado"
+            }
         }
+
+        Snackbar.make(binding.root,texto,Snackbar.LENGTH_SHORT).show()
 
 
         return true
