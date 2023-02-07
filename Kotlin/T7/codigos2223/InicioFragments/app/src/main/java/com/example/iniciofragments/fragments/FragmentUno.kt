6a01606c70669278a126e47d917908ce2ed7e5e0
Develop and System <developandsystem@gmail.com>
2023-02-07 13:34:09 +0100

    update

--- Kotlin/T7/codigos2223/InicioFragments/app/src/main/java/com/example/iniciofragments/fragments/FragmentUno.kt
+++ Kotlin/T7/codigos2223/InicioFragments/app/src/main/java/com/example/iniciofragments/fragments/FragmentUno.kt
@@ -5,8 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.iniciofragments.R
 import com.example.iniciofragments.databinding.FragmentHeaderBinding
 import com.example.iniciofragments.databinding.FragmentUnoBinding
+import com.google.android.material.snackbar.Snackbar
 
 class FragmentUno: Fragment() {
 
@@ -20,4 +22,20 @@ class FragmentUno: Fragment() {
         binding = FragmentUnoBinding.inflate(inflater,container,false)
         return binding.root
     }
+
+
+    override fun onStart() {
+        super.onStart()
+        binding.botonNombreF1.setOnClickListener {
+            //it -> el elemento que provoca el evento boton
+            if (binding.editNombreF1.text.isEmpty()){
+                Snackbar.make(binding.root, "No hay nombre a mostrar",Snackbar.LENGTH_SHORT).show()
+            } else {
+                Snackbar.make(binding.root, binding.editNombreF1.text.toString(),Snackbar.LENGTH_SHORT).show()
+            }
+
+        }
+    }
+
+
 }
