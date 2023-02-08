52aa4186d079ef1993b4a7c90df918921f2ee2e0
Develop and System <developandsystem@gmail.com>
2023-02-08 17:56:17 +0100

    update

--- Kotlin/T7/codigos2223/InicioFragment_ret/app/src/main/java/com/example/iniciofragment_ret/fragments/FragmentNombre.kt
+++ Kotlin/T7/codigos2223/InicioFragment_ret/app/src/main/java/com/example/iniciofragment_ret/fragments/FragmentNombre.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.iniciofragment_ret.databinding.FragmentNombreBinding
+import com.google.android.material.snackbar.Snackbar
 
 class FragmentNombre: Fragment() {
 
@@ -18,6 +19,19 @@ class FragmentNombre: Fragment() {
     ): View? {
         binding = FragmentNombreBinding.inflate(inflater,container,false)
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        binding.botonNombre.setOnClickListener {
+            // it --> boton
+            if (binding.editNombre.text.isEmpty()){
+                Snackbar.make(binding.root, "Nombre vacio",Snackbar.LENGTH_SHORT).show()
+            } else {
+                Snackbar.make(binding.root, binding.editNombre.text.toString(),Snackbar.LENGTH_SHORT).show()
+            }
+        }
     }
 
 }
