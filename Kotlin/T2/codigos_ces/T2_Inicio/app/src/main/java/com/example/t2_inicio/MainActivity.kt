2ef4549f24da1d77ad44492acf968923d6bb2d90
Develop and System <developandsystem@gmail.com>
2023-10-10 12:59:47 +0200

    update

--- Kotlin/T2/codigos_ces/T2_Inicio/app/src/main/java/com/example/t2_inicio/MainActivity.kt
+++ Kotlin/T2/codigos_ces/T2_Inicio/app/src/main/java/com/example/t2_inicio/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.t2_inicio
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,10 +12,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        boton = findViewById(R.id.boton_pulsar);
-        
-        boton.setOnClickListener {
+        instancias()
+        acciones()
 
+
+    }
+
+    private fun acciones() {
+        boton.setOnClickListener {
+            var notificion = Snackbar.make(it, R.string.text_superada, Snackbar.LENGTH_INDEFINITE)
+            notificion.setAction(R.string.text_pulsar){
+                // ocultar la notificacion
+                notificion.dismiss()
+            }
+            notificion.show()
         }
+    }
+
+    private fun instancias() {
+        boton = findViewById(R.id.boton_pulsar);
     }
 }
