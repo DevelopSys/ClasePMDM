db5b053a0e6f8ccf71b40de2e02535dcb0edf866
Develop and System <developandsystem@gmail.com>
2023-10-31 14:10:49 +0100

    update

--- Kotlin/T3/codigos_ces/T3_UI/app/src/main/java/com/example/t3_ui/MainActivity.kt
+++ Kotlin/T3/codigos_ces/T3_UI/app/src/main/java/com/example/t3_ui/MainActivity.kt
@@ -2,15 +2,29 @@ package com.example.t3_ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
 import android.widget.Button
+import com.example.t3_ui.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnClickListener {
 
 
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.botonLimpiar.setOnClickListener(this)
+        binding.botonRegistrar.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            binding.botonLimpiar.id->{}
+            binding.botonRegistrar.id->{}
+        }
     }
 }
