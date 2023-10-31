f0020419093867eb1912030dd024eda47f465c5b
Develop and System <developandsystem@gmail.com>
2023-10-31 14:00:32 +0100

    update

---
+++ Kotlin/T3/codigos_ces/T3_UI/app/src/main/java/com/example/t3_ui/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.t3_ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
