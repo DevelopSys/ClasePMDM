f048db2647170fc0f819c3103899c6a72370c7d7
Develop and System <developandsystem@gmail.com>
2023-11-09 10:59:54 +0100

    update

--- Kotlin/T3/codigos_ces/T3_UI/app/src/main/java/com/example/t3_ui/SecondActivity.kt
+++ Kotlin/T3/codigos_ces/T3_UI/app/src/main/java/com/example/t3_ui/SecondActivity.kt
@@ -4,17 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.View.OnClickListener
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
 import android.widget.ArrayAdapter
 import com.example.t3_ui.databinding.ActivitySecondBinding
+import com.example.t3_ui.model.Marca
 import com.example.t3_ui.model.Usuario
 
-class SecondActivity : AppCompatActivity(), OnClickListener {
+class SecondActivity : AppCompatActivity(), OnClickListener, OnItemSelectedListener {
 
 
     private lateinit var binding: ActivitySecondBinding
     private var usuario: Usuario? = null;
-    private lateinit var listaMarcas: ArrayList<CharSequence>
-    private lateinit var adaptadorMarcas: ArrayAdapter<CharSequence>
+    private lateinit var listaMarcas: ArrayList<Marca>
+    private lateinit var adaptadorMarcas: ArrayAdapter<Marca>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,9 +36,10 @@ class SecondActivity : AppCompatActivity(), OnClickListener {
         super.onStart()
         // cambios graficos
         // poner el correo en su sitio
-        listaMarcas.add("Mercedes")
-        listaMarcas.add("Audi")
-        listaMarcas.add("Ford")
+        listaMarcas.add(Marca("Mercedes",4.9,R.drawable.mercedes))
+        listaMarcas.add(Marca("Audi",4.9,R.drawable.audi))
+        listaMarcas.add(Marca("Ford",4.9,R.drawable.ford))
+
         binding.spinnerMarcas.adapter = adaptadorMarcas
         adaptadorMarcas.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         binding.nombreUsuario.text = usuario?.correo ?: "Invitado"
@@ -46,6 +50,8 @@ class SecondActivity : AppCompatActivity(), OnClickListener {
         // para acciones
         binding.imagenLogout.
         setOnClickListener(this)
+        binding.spinnerMarcas.onItemSelectedListener = this;
+        binding.spinnerModelos.onItemSelectedListener = this;
     }
 
     override fun onClick(v: View?) {
@@ -54,5 +60,19 @@ class SecondActivity : AppCompatActivity(), OnClickListener {
                finish()
             }
         }
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        when(parent?.id){
+            binding.spinnerMarcas.id->{
+               val marca = binding.spinnerMarcas.selectedItem as Marca
+                binding.logoMarca.setImageResource(marca.imagen)
+                binding.textoInfo.text = marca.valoracion.toString()
+            }
+        }
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+
     }
 }
---
+++ Kotlin/T3/codigos_ces/T3_UI/app/src/main/java/com/example/t3_ui/model/Marca.kt
@@ -0,0 +1,8 @@
+package com.example.t3_ui.model
+
+class Marca (var marca: String, var valoracion: Double, var imagen: Int) {
+
+    override fun toString(): String {
+        return marca
+    }
+}
