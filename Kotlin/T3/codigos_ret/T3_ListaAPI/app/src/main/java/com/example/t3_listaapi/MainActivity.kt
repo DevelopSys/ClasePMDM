a004a08e634c4a98d584bbb262a7311dbeb6d0e0
Develop and System <developandsystem@gmail.com>
2023-12-04 18:12:09 +0100

    update

--- Kotlin/T3/codigos_ret/T3_ListaAPI/app/src/main/java/com/example/t3_listaapi/MainActivity.kt
+++ Kotlin/T3/codigos_ret/T3_ListaAPI/app/src/main/java/com/example/t3_listaapi/MainActivity.kt
@@ -3,6 +3,12 @@ package com.example.t3_listaapi
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
+import android.widget.ArrayAdapter
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.android.volley.Request.Method
 import com.android.volley.Response
@@ -16,22 +22,37 @@ import com.example.t3_listaapi.model.Usuario
 import org.json.JSONArray
 import org.json.JSONObject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnItemSelectedListener {
 
     private lateinit var listaUsuario: ArrayList<Usuario>;
     private lateinit var adaptadoUsuariosAdapter: UsuariosAdapter
     private lateinit var binding: ActivityMainBinding
+    private lateinit var adapterResultados: ArrayAdapter<Int>
+    private var numeroSeleccionado: Int = 0;
+    private lateinit var generoSeleccionado: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        adapterResultados = ArrayAdapter(
+            applicationContext,
+            android.R.layout.simple_spinner_item, (1..100).toList()
+        )
+        binding.spinnerResultados.adapter = adapterResultados
+        adapterResultados.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line)
         listaUsuario = ArrayList();
-        adaptadoUsuariosAdapter = UsuariosAdapter(listaUsuario,this)
+        adaptadoUsuariosAdapter = UsuariosAdapter(listaUsuario, this)
         binding.recyclerUsuarios.adapter = adaptadoUsuariosAdapter;
-        binding.recyclerUsuarios.layoutManager = LinearLayoutManager(applicationContext,LinearLayoutManager.VERTICAL,false)
+        binding.recyclerUsuarios.layoutManager =
+            LinearLayoutManager(applicationContext, LinearLayoutManager.VERTICAL, false)
 
 
-        val url = "https://randomuser.me/api/?results=50"
+        binding.spinnerResultados.onItemSelectedListener = this
+        binding.spinnerGenero.onItemSelectedListener = this
+    }
+
+    fun addPetion(url: String): Unit {
+
         // JSONRequest = ctx, url, meth, json, respuesta ok, respuesta err
         val jsonRequest = JsonObjectRequest(url,
             {
@@ -53,34 +74,77 @@ class MainActivity : AppCompatActivity() {
 
         // Vollet. add request
         Volley.newRequestQueue(applicationContext).add(jsonRequest)
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.menu_reset -> {
+                // resetea la peticion ->
+            }
+            R.id.menu_filtros -> {}
+        }
+
+
+        return super.onOptionsItemSelected(item)
     }
 
     fun precesarResponse(response: JSONObject): Unit {
 
+        adaptadoUsuariosAdapter.vaciarLista()
+        val results = response.getJSONArray("results")
+        for (i in 0..results.length() - 1) {
+            val usuario = results.getJSONObject(i)
+            val nombre: String = usuario.getJSONObject("name").getString("first")
+            val apellido: String = usuario.getJSONObject("name").getString("last")
+            val telefono: Long = usuario.getJSONObject("location")
+                .getJSONObject("street").getInt("number")
+                .toLong()
+            /*.toString()
+                .replace(" ","")
+                .replace("(","")
+                .replace(")","")
+                .replace("-","").toLong()*/
+            val mail: String = usuario.getString("email")
+            val imagen: String = usuario.getJSONObject("picture").getString("medium");
+            val pais: String = usuario.getJSONObject("location").getString("country")
+            val ciudad: String = usuario.getJSONObject("location").getString("city")
+            listaUsuario.add(
+                Usuario(
+                    nombre, apellido,
+                    Direccion(pais, ciudad), telefono, mail, imagen
+                )
+            )
+            //adaptadoUsuariosAdapter.notifyDataSetChanged()
+            adaptadoUsuariosAdapter.notifyItemInserted(listaUsuario.size - 1)
+        }
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        // saber que spinner esta seleccionado
+        when (parent!!.id) {
+            binding.spinnerResultados.id -> {
+                numeroSeleccionado = parent.adapter.getItem(position).toString().toInt()
 
 
-            val results = response.getJSONArray("results")
-            for (i in 0..results.length()-1){
-                val usuario = results.getJSONObject(i)
-                val nombre: String = usuario.getJSONObject("name").getString("first")
-                val apellido: String = usuario.getJSONObject("name").getString("last")
-                val telefono: Long = usuario.getJSONObject("location")
-                    .getJSONObject("street").getInt("number")
-                    .toLong()
-                /*.toString()
-                    .replace(" ","")
-                    .replace("(","")
-                    .replace(")","")
-                    .replace("-","").toLong()*/
-                val mail: String = usuario.getString("email")
-                val imagen: String = usuario.getJSONObject("picture").getString("medium");
-                val pais: String = usuario.getJSONObject("location").getString("country")
-                val ciudad: String = usuario.getJSONObject("location").getString("city")
-                listaUsuario.add(Usuario(nombre,apellido,
-                    Direccion(pais,ciudad),telefono,mail, imagen))
-                //adaptadoUsuariosAdapter.notifyDataSetChanged()
-                adaptadoUsuariosAdapter.notifyItemInserted(listaUsuario.size-1)
             }
+
+            binding.spinnerGenero.id -> {
+                generoSeleccionado = parent.adapter.getItem(position).toString()
+            }
+        }
+        val url =
+            "https://randomuser.me/api/?gender=${generoSeleccionado.toLowerCase()}&results=${numeroSeleccionado}"
+        addPetion(url)
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("Not yet implemented")
     }
 }
--- Kotlin/T3/codigos_ret/T3_ListaAPI/app/src/main/java/com/example/t3_listaapi/adapter/UsuariosAdapter.kt
+++ Kotlin/T3/codigos_ret/T3_ListaAPI/app/src/main/java/com/example/t3_listaapi/adapter/UsuariosAdapter.kt
@@ -1,19 +1,24 @@
 package com.example.t3_listaapi.adapter
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.SnapHelper
 import com.bumptech.glide.Glide
 import com.example.t3_listaapi.R
 import com.example.t3_listaapi.model.Usuario
+import com.google.android.material.snackbar.Snackbar
 
-class UsuariosAdapter(var lista: ArrayList<Usuario>, var contexto: Context): RecyclerView.Adapter<UsuariosAdapter.MyHolder>() {
+class UsuariosAdapter(var lista: ArrayList<Usuario>, var contexto: Context) :
+    RecyclerView.Adapter<UsuariosAdapter.MyHolder>() {
 
-    class MyHolder (item: View): RecyclerView.ViewHolder(item){
+    class MyHolder(item: View) : RecyclerView.ViewHolder(item) {
         var textoDetalle: TextView;
         var imagenDetalle: ImageView;
 
@@ -25,7 +30,7 @@ class UsuariosAdapter(var lista: ArrayList<Usuario>, var contexto: Context): Rec
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyHolder {
-       val vista = LayoutInflater.from(contexto).inflate(R.layout.item_recycler,parent,false)
+        val vista = LayoutInflater.from(contexto).inflate(R.layout.item_recycler, parent, false)
         return MyHolder(vista)
     }
 
@@ -33,10 +38,27 @@ class UsuariosAdapter(var lista: ArrayList<Usuario>, var contexto: Context): Rec
         return lista.size
     }
 
+    fun vaciarLista(): Unit {
+        lista.clear();
+        notifyDataSetChanged()
+    }
+
     override fun onBindViewHolder(holder: MyHolder, position: Int) {
 
         val item = lista[position]
-        holder.textoDetalle.text = item.nombre + " "+item.apellido
+        holder.textoDetalle.text = item.nombre + " " + item.apellido
         Glide.with(contexto).load(item.imagen).into(holder.imagenDetalle)
+        holder.imagenDetalle.setOnLongClickListener {
+            Snackbar.make(
+                holder.imagenDetalle,
+                "Quieres realizar la llamada a ${item.telefono}",
+                Snackbar.LENGTH_SHORT
+            ).setAction("LLamar") {
+                    val intent = Intent(Intent.ACTION_CALL,Uri.parse("tel:${item.telefono}"))
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                    contexto.startActivity(intent)
+            }.show()
+            return@setOnLongClickListener true
+        }
     }
 }
