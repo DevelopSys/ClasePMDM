b3ff2188f8d4191d315e6c3f5068bc04f48ed000
Develop and System <developandsystem@gmail.com>
2023-12-05 09:14:54 +0100

    update

--- Kotlin/T3/codigos_ces/app/src/main/java/com/example/t3_listajson/MainActivity.kt
+++ Kotlin/T3/codigos_ces/app/src/main/java/com/example/t3_listajson/MainActivity.kt
@@ -3,17 +3,33 @@ package com.example.t3_listajson
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.android.volley.Request.Method
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
+import com.example.t3_listaapi.adapter.UsuariosAdapter
+import com.example.t3_listajson.databinding.ActivityMainBinding
 import com.example.t3_listajson.model.User
 import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var listaUsuario: ArrayList<User>;
+    private lateinit var adaptadoUsuariosAdapter: UsuariosAdapter
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        listaUsuario = ArrayList();
+        adaptadoUsuariosAdapter = UsuariosAdapter(listaUsuario, this)
+
+        binding.recyclerUsuarios.adapter = adaptadoUsuariosAdapter;
+        binding.recyclerUsuarios.layoutManager =
+            LinearLayoutManager(applicationContext, LinearLayoutManager.VERTICAL, false)
 
         // creo la peticion
         val url = "https://randomuser.me/api/?results=50"
@@ -31,7 +47,7 @@ class MainActivity : AppCompatActivity() {
         // crear clase modelo User
         // obtener una lista de todos los usuarios del JSON
         val results = response.getJSONArray("results")
-        for(i in (0..results.length()-1)){
+        for (i in (0..results.length() - 1)) {
             val user = results.getJSONObject(i);
             val name = user.getJSONObject("name").getString("first")
             val last = user.getJSONObject("name").getString("last")
@@ -39,10 +55,13 @@ class MainActivity : AppCompatActivity() {
             val email = user.getString("email");
             val image = user.getJSONObject("picture").getString("medium")
             val country = user.getJSONObject("location").getString("country")
-            val postCode = user.getJSONObject("location").getInt("postcode")
-            val city = user.getJSONObject("location").getString("city")
-            val usuario = User(name,last,email,phone,postCode,country,city,image)
 
+            val city = user.getJSONObject("location").getString("city")
+            val usuario = User(name, last, email, phone, country, city, image)
+            listaUsuario.add(
+                usuario
+            )
+            adaptadoUsuariosAdapter.notifyItemInserted(listaUsuario.size-1)
         }
         // nombre, apellido, telefono, pais, ciudad, codigo postal, mail
 
--- Kotlin/T3/codigos_ces/app/src/main/java/com/example/t3_listajson/model/User.kt
+++ Kotlin/T3/codigos_ces/app/src/main/java/com/example/t3_listajson/model/User.kt
@@ -1,5 +1,4 @@
 package com.example.t3_listajson.model
 
-class User(val first: String, val last: String, val email: String, val phone: String,
-           val postCoder: Int, val country: String, val city:String, val picture: String) {
+class User(val first: String, val last: String, val email: String, val phone: String, val country: String, val city:String, val picture: String) {
 }
