b8e9f428705f79ed6b32df605724fe0602247ea0
Develop and System <developandsystem@gmail.com>
2023-12-19 12:50:56 +0100

    update

--- Kotlin/T4/codigos_ces/Vuelos/app/src/main/java/com/example/vuelos/MainActivity.kt
+++ Kotlin/T4/codigos_ces/Vuelos/app/src/main/java/com/example/vuelos/MainActivity.kt
@@ -1,17 +1,24 @@
 package com.example.vuelos
 
+import android.app.DatePickerDialog
 import android.app.TimePickerDialog.OnTimeSetListener
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.DatePicker
 import android.widget.TimePicker
+import com.example.vuelos.databinding.ActivityMainBinding
+import com.example.vuelos.ui.dialog.FechaDialog
 import com.example.vuelos.ui.dialog.HoraDialog
 
-class MainActivity : AppCompatActivity(), OnTimeSetListener {
+class MainActivity : AppCompatActivity(), OnTimeSetListener, DatePickerDialog.OnDateSetListener {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -24,7 +31,8 @@ class MainActivity : AppCompatActivity(), OnTimeSetListener {
 
         when(item.itemId){
             R.id.menu_fecha->{
-
+                val fechaDialog = FechaDialog()
+                fechaDialog.show(supportFragmentManager,null)
             }
             R.id.menu_hora->{
                 val dialogoHora = HoraDialog()
@@ -36,6 +44,10 @@ class MainActivity : AppCompatActivity(), OnTimeSetListener {
     }
 
     override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
+        binding.textoHora.text = "$hourOfDay:$minute"
+    }
 
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        binding.textoFecha.text = "$dayOfMonth/${month + 1}/$year"
     }
 }
--- Kotlin/T4/codigos_ces/Vuelos/app/src/main/java/com/example/vuelos/ui/dialog/FechaDialog.kt
+++ Kotlin/T4/codigos_ces/Vuelos/app/src/main/java/com/example/vuelos/ui/dialog/FechaDialog.kt
@@ -1,5 +1,7 @@
 package com.example.vuelos.ui.dialog
 
+import android.app.DatePickerDialog
+import android.app.DatePickerDialog.OnDateSetListener
 import android.app.Dialog
 import android.app.TimePickerDialog
 import android.app.TimePickerDialog.OnTimeSetListener
@@ -8,27 +10,32 @@ import android.os.Bundle
 import androidx.fragment.app.DialogFragment
 import java.util.Calendar
 
-class HoraDialog : DialogFragment() {
+class FechaDialog : DialogFragment() {
 
     private lateinit var contexto: Context
-    private var horaActual = 0;
-    private var minutosActual = 0;
+    private var mesActual = 0;
+    private var diaActual = 0;
+    private var anioActual = 0;
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
         this.contexto = context;
 
         val calendar = Calendar.getInstance()
-        horaActual = calendar.get(Calendar.HOUR_OF_DAY)
-        minutosActual = calendar.get(Calendar.MINUTE)
+        mesActual = calendar.get(Calendar.MONTH)
+        diaActual = calendar.get(Calendar.DAY_OF_MONTH)
+        anioActual = calendar.get(Calendar.YEAR)
 
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         /*buider: title, messaje, positive*/
-        return TimePickerDialog(
-            contexto, contexto as OnTimeSetListener,
-            horaActual, minutosActual, true
+        return DatePickerDialog(
+            contexto,
+            contexto as OnDateSetListener,
+            anioActual,
+            mesActual,
+            diaActual
         )
     }
 
