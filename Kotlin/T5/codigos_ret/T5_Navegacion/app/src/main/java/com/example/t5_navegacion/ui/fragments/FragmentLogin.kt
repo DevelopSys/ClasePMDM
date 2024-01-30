e0fd8a8e23d2da81f054f555c55b77027fe18bbe
Develop and System <developandsystem@gmail.com>
2024-01-30 16:44:59 +0100

    update

--- Kotlin/T5/codigos_ret/T5_Navegacion/app/src/main/java/com/example/t5_navegacion/ui/fragments/FragmentLogin.kt
+++ Kotlin/T5/codigos_ret/T5_Navegacion/app/src/main/java/com/example/t5_navegacion/ui/fragments/FragmentLogin.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.t5_navegacion.R
 import com.example.t5_navegacion.databinding.FragmentLoginBinding
 
 class FragmentLogin : Fragment() {
@@ -14,6 +16,8 @@ class FragmentLogin : Fragment() {
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
+        this.arguments?.getString("nombre")
+        this.arguments?.getString("pass")
     }
 
     override fun onCreateView(
@@ -28,6 +32,12 @@ class FragmentLogin : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.botonLogin.setOnClickListener {
+            findNavController().navigate(R.id.action_fragmentLogin_to_fragmentMain)
+        }
+        binding.botonCuenta.setOnClickListener {
+            findNavController().navigate(R.id.action_fragmentLogin_to_fragmentSignUp)
+        }
     }
 
     override fun onDetach() {
--- Kotlin/T5/codigos_ret/T5_Navegacion/app/src/main/java/com/example/t5_navegacion/ui/fragments/FragmentMain.kt
+++ Kotlin/T5/codigos_ret/T5_Navegacion/app/src/main/java/com/example/t5_navegacion/ui/fragments/FragmentMain.kt
@@ -6,15 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
-import com.example.t5_navegacion.R
 import com.example.t5_navegacion.databinding.FragmentLoginBinding
 import com.example.t5_navegacion.databinding.FragmentMainBinding
 import com.example.t5_navegacion.databinding.FragmentSignupBinding
 
-class FragmentSignUp : Fragment() {
+class FragmentMain : Fragment() {
 
-    private lateinit var binding: FragmentSignupBinding
+    private lateinit var binding: FragmentMainBinding
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -26,14 +24,12 @@ class FragmentSignUp : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        binding = FragmentSignupBinding.inflate(inflater, container, false)
+        binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.botonCrear.setOnClickListener {
-            findNavController().na .navigate(R.id.action_fragmentSignUp_to_fragmentLogin)}
     }
 
     override fun onDetach() {
