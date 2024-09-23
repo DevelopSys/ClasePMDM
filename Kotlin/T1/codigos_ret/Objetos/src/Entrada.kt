9ff65af988f0847e9651a7733d7201e239c62a86
Develop and System <developandsystem@gmail.com>
2024-09-23 17:44:02 +0200

    update

--- Kotlin/T1/codigos_ret/Objetos/src/Entrada.kt
+++ Kotlin/T1/codigos_ret/Objetos/src/Entrada.kt
@@ -2,6 +2,7 @@ import model.Usuario
 
 fun main() {
     // instanciar objetos
+    /*
     var alumno1: Usuario = Usuario("Borja","Martin")
     // nombre = "Borja"
     // apellido = "Martin"
@@ -28,9 +29,47 @@ fun main() {
     // apellido = Aguado
     // correo = luis@aguado.com
     var usuario7: Usuario = Usuario("Luis","Aguado",
-        91222222)
+        "correo",91222222)
     // nombre = Luis
     // apellido = Aguado
     // telefono = 91222222
     // correo = null
+    var usuario8: Usuario = Usuario("Luis","Aguado", telefono = 91222222)*/
+    var alumno1 = Usuario("Borja", "Martin")
+    alumno1.mostrarDatos()
+    var alumno2 = Usuario("Luis", "Gomez", correo = "luis@gmail.com")
+    alumno2.mostrarDatos()
+
+    val alumnos = ArrayList<Usuario>()
+    alumnos.add(alumno2)
+    alumnos.add(alumno1)
+    alumnos.add(Usuario("Claudia","Martin", telefono = 123123))
+    alumnos.add(Usuario("Juan","Lopez",correo = "juan@gmail.com"))
+    alumnos.add(Usuario("Pepe","Gomez"))
+
+    alumnos[3].desmatricular();
+    alumnos[1].desmatricular();
+
+    println("Actualmente la lista de alumnos es de ${alumnos.size}")
+    println("El alumno que esta en la primera posicion es ${alumnos[0].nombre}")
+
+    /*for (i in alumnos) {
+        i.mostrarDatos()
+    }
+
+    for (i in 0 until alumnos.size) {
+        alumnos[i].mostrarDatos()
+    }
+
+    alumnos.forEach { it.mostrarDatos() }*/
+    /*alumnos.forEachIndexed { index, usuario ->
+        //println("En la posicion $index esta ${usuario.nombre}")
+        if(!usuario.matriculado){
+            usuario.mostrarDatos()
+        }
+    }*/
+    // la lista de los matriculados
+    val matriculados: List<Usuario> = alumnos.filter {it.matriculado}
+    // alumnos.filter {it.matriculado}.forEach { it.mostrarDatos() }
+    // diferencia entre un LIST y un ARRAYLIST ->
 }
--- Kotlin/T1/codigos_ret/Objetos/src/model/Usuario.kt
+++ Kotlin/T1/codigos_ret/Objetos/src/model/Usuario.kt
@@ -2,20 +2,57 @@ package model
 
 // CONSTRUCTOR PRIMARIO -> tiene todas las variables que son
 // "obligatorias"
+// un usuario debería tener un ID único obligatorio y una calificacion = 0
+// NECESITAMOS TENER UNA CLASE ADICIONAL QUE GESITIONE TODOS LOS ALUMNOS -> Colegio
+    // agregar un alumno al colegio
+        // no exista -> no puede haber un alumno con el ID que se da
+            // se debe dar aviso del resultado
+    // desmatricular un alumno: id
+            // se debe dar aviso del resultado -> alumno con nombre XXX desmatriculado con exito
+    // listar alumnos
+            // todos
+            // matriculados
+            // desmatriculados
+    // calificar: id, calificacion
+            // si la calificacion no esta en rango -> error
+            // si la calificacion esta en rango -> le pones la nota
+    // mostrar clasificacion: se mostrará una clasificacion con los alumnos ordenador por notas
+
+// Mediante un menu se debe poder ejecutar la funcionalidad -> ENTRADA
+
 class Usuario(var nombre: String? = null, var apellido: String? = null) {
 
+    var lista: ArrayList<Usuario>? = null;
+    // todo el mundo tiene que tener una variable matriculado que se
+    // inicializa a true
     // VARIABLES (LATEINIT o ? -> VAR)
     var correo: String? = null;
     var telefono: Int?=null;
+    var matriculado: Boolean = true;
     // CONSTRUCTOR -> SECUNDARIOS
-    
-    constructor(nombre: String?, apellido: String?, correo: String) : this(nombre, apellido){
-     this.correo = correo;
+
+    init {
+        // ejecutado siempre despues del constructor (primario - secundario)
+        nombre += "_23/24";
     }
-    constructor(nombre: String?, apellido: String?, telefono: Int) : this(nombre, apellido){
+
+    constructor(nombre: String?, apellido: String?, correo: String?=null, telefono: Int?=null) : this(nombre, apellido){
+        this.correo = correo;
         this.telefono = telefono
     }
+
     // FUNCIONES
+    fun mostrarDatos(){
+        println("Nombre: $nombre")
+        println("Apellido: $apellido")
+        println("Matriculado: $matriculado")
+        println("Correo: ${correo?:"sin correo"}")
+        println("Telefono: ${telefono?: "sin telefono"}")
+    }
+
+    fun desmatricular(){
+        this.matriculado = false
+    }
     // GET - SET -> implicitos a no ser que quiera
     // modificar su comportamiento
 }
