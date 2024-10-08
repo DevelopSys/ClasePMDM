7fccf9fb9e1d742e485c2c5543825fe8c44260d2
Develop and System <developandsystem@gmail.com>
2024-10-08 16:40:27 +0200

    update

--- Kotlin/T2/codigos_ces/Contador/app/src/main/java/com/example/contador/MainActivity.kt
+++ Kotlin/T2/codigos_ces/Contador/app/src/main/java/com/example/contador/MainActivity.kt
@@ -10,13 +10,19 @@ import com.example.contador.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var contador: Int=0;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         Log.v("ciclo_vida","ejecutando OnCreate")
         binding.botonIncrementar.setOnClickListener {
-
+            contador++
+            binding.textoContador.text = contador.toString()
+        }
+        binding.botonDecrementar.setOnClickListener {
+            contador--
+            binding.textoContador.text = contador.toString()
         }
 
 
