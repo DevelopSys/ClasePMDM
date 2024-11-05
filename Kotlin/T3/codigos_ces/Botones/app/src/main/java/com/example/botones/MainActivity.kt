455441ee2f30629890021de15f2eb4443b74f682
Develop and System <developandsystem@gmail.com>
2024-11-05 13:17:05 +0100

    update

--- Kotlin/T3/codigos_ces/Botones/app/src/main/java/com/example/botones/MainActivity.kt
+++ Kotlin/T3/codigos_ces/Botones/app/src/main/java/com/example/botones/MainActivity.kt
@@ -1,20 +1,39 @@
 package com.example.botones
 
 import android.os.Bundle
+import android.widget.CompoundButton
+import android.widget.CompoundButton.OnCheckedChangeListener
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.botones.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnCheckedChangeListener {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        acciones()
+    }
+
+
+    private fun acciones() {
+        binding.toggleActivar.setOnCheckedChangeListener(this)
+    }
+
+    override fun onCheckedChanged(buttonView: CompoundButton?, isChecked: Boolean) {
+        when(buttonView?.id){
+            binding.toggleActivar.id->{
+                binding.btnEnviar.isEnabled = isChecked
+            }
         }
     }
 }
