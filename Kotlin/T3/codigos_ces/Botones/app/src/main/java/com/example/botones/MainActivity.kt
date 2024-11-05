beaad92b941d0e5aeee334937cd2b28d8774b851
Develop and System <developandsystem@gmail.com>
2024-11-05 14:07:03 +0100

    update

--- Kotlin/T3/codigos_ces/Botones/app/src/main/java/com/example/botones/MainActivity.kt
+++ Kotlin/T3/codigos_ces/Botones/app/src/main/java/com/example/botones/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.botones
 import android.os.Bundle
 import android.widget.CompoundButton
 import android.widget.CompoundButton.OnCheckedChangeListener
+import android.widget.RadioButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.botones.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity(), OnCheckedChangeListener {
 
@@ -19,7 +21,8 @@ class MainActivity : AppCompatActivity(), OnCheckedChangeListener {
         setContentView(binding.root)
         instancias()
     }
-    fun instancias(){
+
+    fun instancias() {
         listaImagenes = arrayListOf(R.drawable.background2, R.drawable.background1);
     }
 
@@ -35,12 +38,40 @@ class MainActivity : AppCompatActivity(), OnCheckedChangeListener {
         binding.btnSwitch.setOnCheckedChangeListener(this)
         binding.btnEnviar.setOnClickListener {
             binding.imagenHeader.setImageResource(listaImagenes.random())
+
+            when (binding.grupOpcione.checkedRadioButtonId) {
+                binding.radioUno.id -> {}
+                binding.radioDos.id -> {}
+                binding.radioTres.id -> {}
+            }
+
+            val seleccionado: RadioButton? =
+                findViewById(binding.grupOpcione.checkedRadioButtonId)
+
+            Snackbar.make(
+                binding.root,
+                seleccionado?.text ?: "No hay seleccion", Snackbar.LENGTH_SHORT
+            ).show()
+        }
+        binding.grupOpcione.setOnCheckedChangeListener { _, checkedId ->
+
+            when(checkedId){
+                binding.radioUno.id->{
+                    binding.imagenHeader.setImageResource(R.drawable.background1)
+                }
+                binding.radioDos.id->{
+                    binding.imagenHeader.setImageResource(R.drawable.background2)
+                }
+                binding.radioTres.id->{
+                    binding.imagenHeader.setImageResource(R.drawable.ic_launcher_background)
+                }
+            }
         }
     }
 
     override fun onCheckedChanged(buttonView: CompoundButton?, isChecked: Boolean) {
-        when(buttonView?.id){
-            binding.toggleActivar.id->{
+        when (buttonView?.id) {
+            binding.toggleActivar.id -> {
                 binding.btnEnviar.isEnabled = isChecked
             }
         }
