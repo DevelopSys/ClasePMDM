5f0b2aecc28b6360143a877f03dbd4ff9523357d
Develop and System <developandsystem@gmail.com>
2025-02-04 16:18:57 +0100

    update

--- Kotlin/T5/codigos_ret/navegacion/app/build.gradle.kts
+++ Kotlin/T5/codigos_ret/navegacion/app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 
 dependencies {
 
+    implementation(libs.glide)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
---
+++ Kotlin/T5/codigos_ret/navegacion/app/src/main/java/com/example/navegacion/ui/fragments/FragmentRegister.kt
@@ -0,0 +1,32 @@
+package com.example.navegacion.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.navegacion.R
+import com.example.navegacion.databinding.FragmentRegisterBinding
+
+class FragmentRegister : Fragment() {
+
+    private lateinit var binding: FragmentRegisterBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentRegisterBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.btnRegistrar.setOnClickListener {
+            findNavController().navigate(R.id.action_fragmentRegister_to_fragmentLogin)
+        }
+    }
+
+}
