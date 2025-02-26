add0c3efe225bcac273dfad1abb3557c9a954ab9
Develop and System <developandsystem@gmail.com>
2025-02-26 17:50:03 +0100

    update

---
+++ Kotlin/T6/codigos_ret/navegacion/app/src/main/java/com/example/navegacion/model/Producto.kt
@@ -0,0 +1,4 @@
+package com.example.navegacion.model
+
+class Producto(var id: Int?=null,var nombre: String?=null, var precio: Double?=null, var descripcion: String?=null) {
+}
--- Kotlin/T6/codigos_ret/navegacion/app/src/main/java/com/example/navegacion/ui/fragments/FragmentMain.kt
+++ Kotlin/T6/codigos_ret/navegacion/app/src/main/java/com/example/navegacion/ui/fragments/FragmentMain.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.navegacion.databinding.FragmentMainBinding
+import com.example.navegacion.model.Producto
 import com.example.navegacion.model.Usuario
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.ChildEventListener
@@ -42,10 +43,18 @@ class FragmentMain : Fragment() {
         super.onStart()
         binding.btnEscribir.setOnClickListener {
 
-            val usuario = Usuario("Borja1", "borja@gmail.com", "1234")
+            /*val usuario = Usuario("Borja1", "borja@gmail.com", "1234")
             val referencia = database.reference.child("usuarios")
                 .child(auth.currentUser!!.uid)
-            referencia.setValue(usuario)
+            referencia.setValue(usuario)*/
+
+            val producto = Producto(2, "Producto2", 20.76, "Este producto es uno de mis favoritos2")
+
+            val referencia = database.reference.child("usuarios")
+                .child(auth.currentUser!!.uid).child("favs")
+                .child(producto.id.toString()).setValue(producto)
+
+
         }
         binding.btnLeer.setOnClickListener {
             /*database.reference.child("version")
@@ -55,46 +64,28 @@ class FragmentMain : Fragment() {
                         Log.v("lectura",it.result.value.toString())
                     }
                 }*/
-            database.reference.child("usuarios")
-                .addChildEventListener(object : ChildEventListener{
+            /*database.reference.child("usuarios")
+                .addChildEventListener(object : ChildEventListener {
                     override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
-                        Log.v("lectura","Añadido "+snapshot.toString())
-                        Log.v("lectura","Previo "+previousChildName)
+                        Log.v("lectura", "Añadido " + snapshot.toString())
+                        Log.v("lectura", "Previo " + previousChildName)
                     }
 
                     override fun onChildChanged(
                         snapshot: DataSnapshot,
                         previousChildName: String?
                     ) {
-                        Log.v("lectura","Cambiado "+snapshot.toString())
-                        Log.v("lectura","Previo "+previousChildName)
+                        Log.v("lectura", "Cambiado " + snapshot.toString())
+                        Log.v("lectura", "Previo " + previousChildName)
                     }
 
                     override fun onChildRemoved(snapshot: DataSnapshot) {
-                        Log.v("lectura","Borrado "+snapshot.toString())
+                        Log.v("lectura", "Borrado " + snapshot.toString())
                     }
 
                     override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
-                        Log.v("lectura","Movido "+snapshot.toString())
-                        Log.v("lectura","Previo "+previousChildName)
-                    }
-
-                    override fun onCancelled(error: DatabaseError) {
-
-                    }
-
-                })
-
-                /*.addValueEventListener(object : ValueEventListener {
-                    override fun onDataChange(snapshot: DataSnapshot) {
-                        for (i in snapshot.children) {
-
-                            val usuario = i.getValue(Usuario::class.java)
-
-                            Log.v("lectura", usuario!!.nombre.toString())
-                            Log.v("lectura", usuario!!.correo.toString())
-                            Log.v("lectura", usuario!!.pass.toString())
-                        }
+                        Log.v("lectura", "Movido " + snapshot.toString())
+                        Log.v("lectura", "Previo " + previousChildName)
                     }
 
                     override fun onCancelled(error: DatabaseError) {
@@ -102,6 +93,51 @@ class FragmentMain : Fragment() {
                     }
 
                 })*/
+            /*.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    for (i in snapshot.children) {
+
+                        val usuario = i.getValue(Usuario::class.java)
+
+                        Log.v("lectura", usuario!!.nombre.toString())
+                        Log.v("lectura", usuario!!.correo.toString())
+                        Log.v("lectura", usuario!!.pass.toString())
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+
+                }
+
+            })*/
+            database.reference.child("usuarios").child(auth.currentUser!!.uid)
+                .child("favs").addChildEventListener(object : ChildEventListener {
+                    override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+                        val producto = snapshot.getValue(Producto::class.java)
+                        Log.v("datos", producto!!.nombre!!)
+
+                    }
+
+                    override fun onChildChanged(
+                        snapshot: DataSnapshot,
+                        previousChildName: String?
+                    ) {
+                        TODO("Not yet implemented")
+                    }
+
+                    override fun onChildRemoved(snapshot: DataSnapshot) {
+                        TODO("Not yet implemented")
+                    }
+
+                    override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
+                        TODO("Not yet implemented")
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {
+                        TODO("Not yet implemented")
+                    }
+
+                })
         }
     }
 }
