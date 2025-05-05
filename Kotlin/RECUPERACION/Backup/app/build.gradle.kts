4d3b79d4b19ecbeffe7adb98643635026b0f3d69
Develop and System <developandsystem@gmail.com>
2025-05-05 16:06:26 +0200

    update

--- Kotlin/RECUPERACION/Backup/app/build.gradle.kts
+++ Kotlin/RECUPERACION/Backup/app/build.gradle.kts
@@ -1,15 +1,14 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
-    namespace = "com.example.recuperacion"
+    namespace = "com.example.backup"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.recuperacion"
+        applicationId = "com.example.backup"
         minSdk = 26
         targetSdk = 35
         versionCode = 1
@@ -34,9 +33,6 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
-    buildFeatures {
-        viewBinding = true
-    }
 }
 
 dependencies {
@@ -44,10 +40,8 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.navigation.fragment.ktx)
-    implementation(libs.androidx.navigation.ui.ktx)
-    implementation(libs.firebase.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
