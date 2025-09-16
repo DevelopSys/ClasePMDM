be27f04e5a865f32b85e72ac17690eebd9fd8173
Develop and System <developandsystem@gmail.com>
2025-09-16 14:30:36 +0200

    update

--- Kotlin/T1/codigos_ces/Inicio/src/Entrada.kt
+++ Kotlin/T1/codigos_ces/Inicio/src/Entrada.kt
@@ -1,4 +1,4 @@
-var sumaFlecha: ((Int,Int) -> Unit) = { p1: Int, p2: Int ->
+var sumaFlecha: ((Int, Int) -> Unit)? = { p1: Int, p2: Int ->
     println("El resultado de la suma es ${p1 + p2}")
 }
 
@@ -26,12 +26,54 @@ fun main() {
     println("Mi direccion es $direccion")
     */
     //println("El resultado de la operacion es ${sumar(null, null)}")
-    sumaFlecha = null
-    sumaFlecha.invoke(1, 55) ?: "asdasdasd"
+    // sumaFlecha = null
+    // sumaFlecha?.invoke(1, 55) ?: "asdasdasd"
+    // decidirWhen(9)
+    repetirFor()
 }
+
 fun saludar(nombreParam: String = "Sin nombre", vecesParam: Int) {
     println("Hola $nombreParam, acabas de realizar una funcion")
 }
+
 fun sumar(op1: Int = 0, op2: Int = 0): Int {
     return op1.and(op2)
+}
+
+fun decidirWhen(valor: Int) {
+    println("El valor analizado es:")
+    when (valor) {
+        1 -> {
+            println("El caso tiene como valor 1")
+        }
+
+        2 -> {
+            println("El caso tiene como valor 2")
+        }
+
+        3 -> {
+            println("El caso tiene como valor 3")
+        }
+
+        4 -> {
+            println("El caso tiene como valor 4")
+        }
+
+        else -> println("Caso no contemplado")
+
+    }
+}
+
+fun repetirFor() {
+
+    /*for (i in 0..9){
+        println(i)
+    }*/
+
+    // si la funcion de flecha solo tiene 1 ARGUMENTO
+    // -> este se puede llamar con la palabra reservada it
+    //(20..30).forEach { p -> println(p) }
+    (20..30).forEachIndexed { index, item ->
+        println("Elemento en posicion $index con valor determinado")
+    }
 }
