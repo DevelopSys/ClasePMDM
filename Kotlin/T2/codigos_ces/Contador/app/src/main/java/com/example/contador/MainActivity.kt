5a557e4b37d04ef4c32178fec1050498c43a6ca6
Develop and System <developandsystem@gmail.com>
2025-10-24 09:00:33 +0200

    update

---
+++ Kotlin/T2/codigos_ces/Contador/app/src/main/java/com/example/contador/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.contador
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.contador.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    var contador: Int = 0;
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        contador = savedInstanceState?.getInt("tag_contador") ?:0
+        binding.textoContador.text = contador.toString()
+
+        acciones()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("tag_contador",contador)
+    }
+
+    private fun acciones() {
+        binding.botonIncremento.setOnClickListener(this)
+        binding.botonDecremento.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        // diferenciar quien ha lanzado el evento
+        // if(v!!.id == binding.botonIncremento.id)
+        when (v!!.id) {
+            binding.botonDecremento.id -> {
+                contador--
+            }
+
+            binding.botonIncremento.id -> {
+                contador++
+            }
+        }
+        binding.textoContador.text = contador.toString()
+    }
+}
