b744827e52dbd76dd898a71248d8c3e2bb4a9df4
Develop and System <developandsystem@gmail.com>
2025-10-29 09:13:35 +0100

    update

---
+++ Kotlin/T2/codigos_ret/app/src/main/java/com/example/formulario/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.formulario
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.formulario.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
