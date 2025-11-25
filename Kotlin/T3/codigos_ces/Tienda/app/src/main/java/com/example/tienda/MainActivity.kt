f5ff620cf3ec74f9479c1bc533836173ffc0ad8e
Develop and System <developandsystem@gmail.com>
2025-11-25 12:57:43 +0100

    update

--- Kotlin/T3/codigos_ces/Tienda/app/src/main/java/com/example/tienda/MainActivity.kt
+++ Kotlin/T3/codigos_ces/Tienda/app/src/main/java/com/example/tienda/MainActivity.kt
@@ -5,16 +5,41 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.tienda.adapter.AdapterProducto
+import com.example.tienda.databinding.ActivityMainBinding
+import com.example.tienda.model.Producto
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapterProducto: AdapterProducto
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val lista: ArrayList<Producto> =
+            arrayListOf(
+                Producto(1, "Nombre1", 12, 10.0, "descripcion", "muebles", ""),
+                Producto(2, "Nombre2", 12, 10.0, "descripcion", "muebles", ""),
+                Producto(2, "Nombre2", 12, 10.0, "descripcion", "muebles", ""),
+                Producto(2, "Nombre2", 12, 10.0, "descripcion", "muebles", ""),
+                Producto(2, "Nombre2", 12, 10.0, "descripcion", "muebles", ""),
+                Producto(2, "Nombre2", 12, 10.0, "descripcion", "muebles", ""),
+                Producto(2, "Nombre2", 12, 10.0, "descripcion", "muebles", ""),
+                Producto(2, "Nombre2", 12, 10.0, "descripcion", "muebles", ""),
+                Producto(2, "Nombre2", 12, 10.0, "descripcion", "muebles", ""),
+                Producto(2, "Nombre2", 12, 10.0, "descripcion", "muebles", ""),
+                Producto(2, "Nombre2", 12, 10.0, "descripcion", "muebles", ""),
+                Producto(2, "Nombre2", 12, 10.0, "descripcion", "muebles", ""),
+                Producto(2, "Nombre2", 12, 10.0, "descripcion", "muebles", ""),
+                Producto(2, "Nombre2", 12, 10.0, "descripcion", "muebles", ""),
+
+            )
+        adapterProducto = AdapterProducto(lista, this)
+        binding.recyclerProductos.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        binding.recyclerProductos.adapter = adapterProducto;
+
     }
 }
--- Kotlin/T3/codigos_ces/Tienda/app/src/main/java/com/example/tienda/adapter/AdapterProducto.kt
+++ Kotlin/T3/codigos_ces/Tienda/app/src/main/java/com/example/tienda/adapter/AdapterProducto.kt
@@ -1,7 +1,41 @@
 package com.example.tienda.adapter
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.tienda.databinding.ItemProductoBinding
+import com.example.tienda.model.Producto
 
-class AdapterProducto  {
-    inner class MyHolder()
+class AdapterProducto(var lista: ArrayList<Producto>, var contexto: Context) :
+    RecyclerView.Adapter<AdapterProducto.MyHolder>() {
+    inner class MyHolder(var binding: ItemProductoBinding) : RecyclerView.ViewHolder(binding.root)
+
+    // crea un holder de la clase anidada
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): MyHolder {
+        var binding: ItemProductoBinding =
+            ItemProductoBinding.inflate(LayoutInflater.from(contexto), parent, false)
+        return MyHolder(binding)
+    }
+
+    // asociar los elementos (posicion) con el holder asociado
+    override fun onBindViewHolder(
+        holder: MyHolder,
+        position: Int
+    ) {
+        val producto: Producto = lista[position]
+        holder.binding.nombreFila.text = producto.nombre
+        holder.binding.btnDetalle.setOnClickListener {  }
+        holder.binding.btnCompra.setOnClickListener {  }
+    }
+
+    // cuantos elementso tendre que pintar
+    override fun getItemCount(): Int {
+        return lista.size
+    }
+
+
 }
