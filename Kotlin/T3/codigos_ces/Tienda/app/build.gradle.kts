0b6c1d341ad75d626575c62b69828d96705a4a4b
Develop and System <developandsystem@gmail.com>
2025-11-25 12:34:03 +0100

    update

--- Kotlin/T3/codigos_ces/Tienda/app/build.gradle.kts
+++ Kotlin/T3/codigos_ces/Tienda/app/build.gradle.kts
@@ -35,6 +35,10 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+
+    viewBinding{
+        enable = true
+    }
 }
 
 dependencies {
---
+++ Kotlin/T3/codigos_ces/Tienda/app/src/main/java/com/example/tienda/adapter/AdapterProducto.kt
@@ -0,0 +1,7 @@
+package com.example.tienda.adapter
+
+import androidx.recyclerview.widget.RecyclerView
+
+class AdapterProducto  {
+    inner class MyHolder()
+}
