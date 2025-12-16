1a908624aeacfbf074969ca9ff342517b804fb11
Develop and System <developandsystem@gmail.com>
2025-12-16 10:27:48 +0100

    Update DIalogoComparar.kt

--- Kotlin/T3/codigos_ces/Tienda/app/src/main/java/com/example/tienda/ui/dialogs/DIalogoComparar.kt
+++ Kotlin/T3/codigos_ces/Tienda/app/src/main/java/com/example/tienda/ui/dialogs/DIalogoComparar.kt
@@ -7,15 +7,20 @@ import androidx.fragment.app.DialogFragment
 
 class DIalogoComparar : DialogFragment() {
 
+    private var opcionsSeleccionada: String? = null
+
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val builder: AlertDialog.Builder = AlertDialog.Builder(requireContext())
         val opciones: Array<CharSequence> = arrayOf("Precio", "Categoria", "Stock")
         builder.setTitle("Comparar productos")
         builder.setSingleChoiceItems(opciones, -1) { view, position ->
-
+            opcionsSeleccionada = opciones[position].toString()
         }
 
         builder.setPositiveButton("Seleccionar") { view, position ->
+            if(opcionsSeleccionada!=null){
+
+            }
             dismiss()
         }
         return builder.create()
